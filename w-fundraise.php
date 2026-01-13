3c527a6f0d35f6a8c6a789363bd3c24baf761d0e
Thamara111 <thamara.dasun1@gmail.com>
2026-01-13 20:06:39 +0530

    aos scroll

--- w-fundraise.php
+++ w-fundraise.php
@@ -19,7 +19,7 @@ require_once 'layouts/header.php';
 <!-- Contact Section -->
 <section class="">
   <div class="container mx-auto px-6 py-12">
-    <div class="text-center mb-12">
+    <div class="text-center mb-12" data-aos="fade-down" data-aos-duration="1500">
       <h2 class="text-4xl md:text-6xl font-bold mb-4">Fundraising</h2>
       <p class="text-gray-500 mb-12 max-w-2xl mx-auto">
         Your support empowers us to create lasting change for elderly
@@ -28,7 +28,7 @@ require_once 'layouts/header.php';
         enhance their quality of life.
       </p>
     </div>
-    <div id="fund-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+    <div id="fund-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" data-aos="fade-up" data-aos-duration="1500"></div>
     <div id="loading-state" class="text-center text-gray-500 mt-8">
       Loading funds...
     </div>
